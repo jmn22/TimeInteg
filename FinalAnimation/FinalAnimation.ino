05544b8a2ff65ed3b365ddb97313892c60b75b9b
Virginia Chen <virginia.chen@duke.edu>
2013-09-05 17:58:05 -0400

    Uploaded Final Animation
    rough EpiCool logo

---
+++ FinalAnimation/FinalAnimation.ino
@@ -0,0 +1,23 @@
+
+
+#include <ColorLCDShield.h>
+
+LCDShield lcd;  // Creates an LCDShield, named lcd
+
+void setup()
+{
+  lcd.init(PHILIPS);  // Initializes lcd, using an PHILIPSdriver
+  lcd.contrast(-51);  // -51's usually a good contrast value
+  lcd.clear(WHITE);  // clear the screen
+  
+  lcd.setStr("EpiCool", 110, 10, SLATE, WHITE);
+  lcd.setCircle(66, 66, 50, BLUE, 3);
+  lcd.setLine(52,85,80,95,BLUE);
+  lcd.setLine(52,95,80,85,BLUE);
+  lcd.setLine(41,90,91,90,BLUE);
+  lcd.setLine(66,100,66,80,BLUE);
+}
+
+void loop()
+{
+}
