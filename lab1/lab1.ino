b222ea6b58896600e3b3012c07dc28cb228d1737
carolineseng <caroline.seng@duke.edu>
2013-09-05 18:21:25 -0400

    Changed method for button reading

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -6,6 +6,8 @@ int buttons[3] = {3, 4, 5};  // S1 = 3, S2 = 4, S3 = 5
 signed char cont = -51;  // Philips medium contrast
 boolean b1 = 0;
 boolean b2 = 0;
+int timeStart = 0;
+int timeCount = 0;
 
 void setup()
 {
@@ -22,15 +24,31 @@ void setup()
 
 void loop()
 {
-  // If max brightness achieved, do shindig
-  // Check for button presses
-  b1 = digitalRead(buttons[0]);
-  b2 = digitalRead(buttons[1]);
-  if(b1&&b2) {
-    // do error/nothing
-  } else if(b1) {
-    // incrememnt
-  } else if(b2) {
-    // decrement
+  // display current time
+  
+  // If max time achieved, do shindig
+  if(timeCount > 30000) {
+    while (1) {
+      // Do shindig 
+    }
   }
+  
+  // Check for button presses
+  boolean firstTime = 1;
+  while (millis() % 100) {
+    while (firstTime) {
+      b1 = digitalRead(buttons[0]);
+      b2 = digitalRead(buttons[1]);
+      if(b1&&b2) {
+        // do error/nothing
+      } else if(b1) {
+        // incrememnt
+      } else if(b2) {
+        // decrement
+      }
+      firstTime = 0;
+    }
+  }
+  // Write value to display
+  
 }
